15e89fb63047331d82140e59d008fff3c2b9c3b8
bruno <bruno@ubuntuserver.home>
2025-12-23 21:21:17 +0100

    Remove all callback functions to fix migration error - rely solely on template override

--- local/afirws/lib.php
+++ local/afirws/lib.php
@@ -6,10 +6,4 @@ defined('MOODLE_INTERNAL') || die();
  * AFIR Web Services local plugin library functions
  */
 
-/**
- * Hook to redirect non-logged users to landing page
- * This function is called during Moodle initialization
- */
-function local_afirws_extend_navigation(global_navigation $nav) {
-    // This hook is called early in page setup
-}
+// No callback functions - using template override for homepage redirection
