3c23afb1ca9cc1ece62fe4e6410b6806ac5f5768
bruno <bruno@ubuntuserver.home>
2025-12-23 21:44:46 +0100

    Ajout du fichier version.php manquant pour le plugin local_afirws

---
+++ local/afirws/version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details for the AFI Remote WS plugin
+ *
+ * @package    local_afirws
+ * @copyright  2025 AFI Formation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_afirws';
+$plugin->version = 2024122300;  // YYYYMMDDXX
+$plugin->requires = 2023100900; // Moodle 4.3.0 et supérieur
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
