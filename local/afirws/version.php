f7a58ba1a03910d5db3f35d1bb6fb34855845f09
Bruno <bruno@afir-formation.fr>
2025-12-27 00:14:30 +0100

    Retour au système de hooks legacy pour compatibilité

--- local/afirws/version.php
+++ local/afirws/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_afirws';
-$plugin->version = 2025122015;  // Correction de l'implémentation du hook (pas d'interface)
+$plugin->version = 2025122016;  // Retour au système de hooks legacy pour compatibilité
 $plugin->requires = 2023100900; // Moodle 4.3.0 et supérieur
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
